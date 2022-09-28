155ecb9e3b09b1934153dba415aa08c62bf900b1
Solar5503 <spidervvv@mail.ru>
2022-09-28 21:48:49 +0300

    add handling dynamic routes and  error processing

--- src/pages/RootLayout.jsx
+++ src/pages/RootLayout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import MainNavigation from '../components/MainNavigation';
 
-function RootLayout({ children }) {
+function RootLayout() {
   return (
     <>
       <MainNavigation />
